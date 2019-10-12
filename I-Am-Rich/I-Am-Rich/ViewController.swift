cbbbaf80326159c25e546d7c694f1b1b543a0a6f
saiwaimaung <saiwaimaung@gmail.com>
2019-10-12 22:03:29 +0800

    I Am Rich clone

---
+++ I-Am-Rich/I-Am-Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I-Am-Rich
+//
+//  Created by Sai Wai Maung on 2019-10-12.
+//  Copyright © 2019 io.saimaung.github. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
